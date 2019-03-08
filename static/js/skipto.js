1e09c0a41fca35b91b9257716f14b5961ae17cc5
Benzer Bett <benzerbett@gmail.com>
2019-03-08 15:41:25 +0300

    skip & undo implemented for Q5

--- static/js/skipto.js
+++ static/js/skipto.js
@@ -19,7 +19,11 @@ function triggerSkip(inputToCheck,rightValue,toQnID,toTabID) {
         if($('input[name="'+inputToCheck+'"]').attr('type') == 'date' || $('input[name="'+inputToCheck+'"]').attr('type') == 'text' || $('input[name="'+inputToCheck+'"]').attr('type') == 'number'){
             var valFromInput = $('input[name="'+inputToCheck+'"]').val();
             if(valFromInput == rightValue){
-                skipToQn(inputToCheck,toQnID,toTabID);
+                var unDo = false
+                skipToQn(inputToCheck,toQnID,toTabID,unDo);
+            }else{
+                var unDo = true;
+                skipToQn(inputToCheck,toQnID,toTabID,unDo);
             }
         }
         // END textbox, numbers, dates etc
@@ -31,9 +35,12 @@ function triggerSkip(inputToCheck,rightValue,toQnID,toTabID) {
             $.each($(this), function (chind, eachbx) {
                 if($(this).is(':checked')){
                     console.log("TICKED checkbox with Name: "+inputToCheck+" found");
-                    skipToQn(inputToCheck,toQnID,toTabID);
-                    // var eachbxval = eachbx.val();
-                    // valFromInput += eachbxval+', ';
+                    var unDo = false;
+                    skipToQn(inputToCheck,toQnID,toTabID,unDo);
+                }else{
+                    console.log("UNDO ticked checkbox with Name: "+inputToCheck);
+                    var unDo = true;
+                    skipToQn(inputToCheck,toQnID,toTabID,unDo);
                 }
             });
         }
@@ -45,22 +52,35 @@ function triggerSkip(inputToCheck,rightValue,toQnID,toTabID) {
             var valFromInput = $('input[name="'+inputToCheck+'"]:checked').val();
             if(valFromInput == rightValue){
                 console.log("TICKED radio with Name: "+inputToCheck+" found");
-                skipToQn(inputToCheck,toQnID,toTabID);
+                var unDo = false;
+                skipToQn(inputToCheck,toQnID,toTabID,unDo);
+            }else{
+                var unDo = true;
+                skipToQn(inputToCheck,toQnID,toTabID,unDo);
             }
         }
         //END cater for radio
     });
 }
-function skipToQn(inputToCheck,toQnID,toTabID) {
+function skipToQn(inputToCheck,toQnID,toTabID,unDo) {
     $('a[href="#step'+toTabID+'"]').trigger("click");
-    $("td").attr("tabindex", "-1");
-    $('input[name="'+toQnID+'"]').closest("td").attr("tabindex", "1");
-    $('input[name="'+toQnID+'"]').closest("td").focus();
     //hideQnsBtwn
-        var destinationT = $('input[name="'+toQnID+'"]').closest("tr");
-        $('input[name="'+inputToCheck+'"]').closest("tr").nextUntil(destinationT, "tr").addClass('hidden');
+    var destinationT = $('input[name="'+toQnID+'"]').closest("tr");
+    if(!unDo){
+            $("td").attr("tabindex", "-1");
+            $('input[name="'+toQnID+'"]').closest("td").attr("tabindex", "1");
+            $('input[name="'+toQnID+'"]').closest("td").focus();
+            $('input[name="'+inputToCheck+'"]').closest("tr").nextUntil(destinationT, "tr").addClass('hidden');
+            $('input[name="'+toQnID+'"]').closest("td").css('outline', 'thick double #32a1ce');
+            console.log("skipping to Qn: "+toQnID+" on Tab: "+toTabID);
+        }else{
+            $("td").attr("tabindex", "-1");
+            $('input[name="'+inputToCheck+'"]').closest("td").attr("tabindex", "1");
+            $('input[name="'+inputToCheck+'"]').closest("td").focus();
+            $('input[name="'+inputToCheck+'"]').closest("tr").nextUntil(destinationT, "tr").removeClass('hidden');
+            $('input[name="'+inputToCheck+'"]').closest("td").css('outline', 'thick double #32a1ce');
+            console.log("UNDO skipping to Qn: "+toQnID+" on Tab: "+toTabID);
+        }
     //hideQnsBtwn
-    $('input[name="'+toQnID+'"]').closest("td").css('outline', 'thick double #32a1ce');
-    console.log("skipping to Qn: "+toQnID+" on Tab: "+toTabID);
 }
 // ------------------------endCORE-------------------------
