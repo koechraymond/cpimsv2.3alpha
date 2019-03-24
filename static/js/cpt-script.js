bd4473f66bd6d03a2b2d2146de3072bc174bc48a
Benzer Bett <benzerbett@gmail.com>
2019-03-24 12:36:54 +0300

    caseplantemplate added hidden inputs, table listing

--- static/js/cpt-script.js
+++ static/js/cpt-script.js
@@ -15,68 +15,18 @@ jQuery(document).ready(function()
 	FormWizardValidation.init();
 });
 
+function randomNo() {
+    var min=101; 
+    var max=1999;  
+    var random = Math.random() * (+max - +min) + +min; 
+    random = random.toFixed(0);
+    return random;
+}
 
-// let cpt_domain_choices = $('select[name=CPT_DOMAIN_CHOICES]');
-var cpt_domain_choices = {};
-cpt_domain_choices['val'] = [];
-cpt_domain_choices['text'] = [];
-$("select[name=CPT_DOMAIN] option").each(function(){
-    cpt_domain_choices['val'].push($(this).val());
-    cpt_domain_choices['text'].push($(this).text());
-});
-// let cpt_goal_choices = $('select[name=CPT_GOALS_CHOICES]');
-var cpt_goal_choices = {};
-cpt_goal_choices['val'] = [];
-cpt_goal_choices['text'] = [];
-$("select[name=CPT_GOALS] option").each(function(){
-    cpt_goal_choices['val'].push($(this).val());
-    cpt_goal_choices['text'].push($(this).text());
-});
-
-// let cpt_gaps_healthy_choices = $('select[name=CPT_GAPS_HEALTHY_CHOICES]');
-var cpt_gaps_healthy_choices = {};
-cpt_gaps_healthy_choices['val'] = [];
-cpt_gaps_healthy_choices['text'] = [];
-$("select[name=CPT_GAPS] option").each(function(){
-    cpt_gaps_healthy_choices['val'].push($(this).val());
-    cpt_gaps_healthy_choices['text'].push($(this).text());
-});
-// let cpt_actions_healthy_choices = $('select[name=CPT_ACTIONS_HEALTHY_CHOICES]');
-var cpt_actions_healthy_choices = {};
-cpt_gaps_healthy_choices['val'] = [];
-cpt_gaps_healthy_choices['text'] = [];
-$("select[name=CPT_ACTIONS] option").each(function(){
-    cpt_gaps_healthy_choices['val'].push($(this).val());
-    cpt_gaps_healthy_choices['text'].push($(this).text());
-});
-// let cpt_services_healthy_choices = $('select[name=CPT_SERVICES_HEALTHY_CHOICES]');
-var cpt_services_healthy_choices = {};
-cpt_services_healthy_choices['val'] = [];
-cpt_services_healthy_choices['text'] = [];
-$("select[name=CPT_SERVICES] option").each(function(){
-    cpt_services_healthy_choices['val'].push($(this).val());
-    cpt_services_healthy_choices['text'].push($(this).text());
-});
-// let cpt_person_responsible = $('select[name=CPT_PERSON_RESPONSIBLE]');
-var cpt_person_responsible = {};
-cpt_person_responsible['val'] = [];
-cpt_person_responsible['text'] = [];
-$("select[name=CPT_RESPONSIBLE] option").each(function(){
-    cpt_person_responsible['val'].push($(this).val());
-    cpt_person_responsible['text'].push($(this).text());
-});
-// let cpt_results = $('select[name=CPT_RESULTS]');
-var cpt_results = {};
-cpt_results['val'] = []
-cpt_results['text'] = []
-$("select[name=CPT_RESULTS] option").each(function(){
-    cpt_results['val'].push($(this).val());
-    cpt_results['text'].push($(this).text());
-});
-
-
-
-function AddRow() {
+function AddRow() {    
+    var randomID = randomNo();
+    $('#submissions_table tbody').append('<tr id="row_'+randomID+'"> <td id="tbl_domain"></td> <td id="tbl_goal"></td> <td id="tbl_needs"></td> <td id="tbl_actions"></td> <td id="tbl_services"></td> <td id="tbl_repsonsible"></td> <td id="tbl_datecompleted"></td> <td id="tbl_results"></td> <td id="tbl_reasons"></td> <td id="tbl_acts"></td></tr>');
+    
     let domain = $('#id_CPT_DOMAIN option:selected').val();
     let goal = $('#id_CPT_GOAL').val();
     let gaps = $('#id_CPT_GAPS option:selected').val();
@@ -87,26 +37,29 @@ function AddRow() {
     let results = $('#id_CPT_RESULTS option:selected').val();
     let reasons = $('#id_CPT_REASONS').val();
 
-    $('#tbl_domain').html( $('select[name=CPT_DOMAIN] option[value='+domain+']').text() );
+    $('#row_'+randomID+' > td#tbl_domain').html( $('select[name=CPT_DOMAIN] option[value='+domain+']').text() + '<input type="hidden" name="h_CPT_DOMAIN" value="'+domain+'" />');
     // console.log("goal == "+goal);
-    $('#tbl_goal').empty();
+    $('#row_'+randomID+' > td#tbl_goal').empty();
+    if(goal.length > 2){ $('#row_'+randomID+' > td#tbl_goal').append('<ul class="ul-flow">'); }
     for (let i = 0; i < goal.length; i++) {
-        $('#tbl_goal').append('<li>'+ $("select[name=CPT_GAPS] option[value="+goal[i]+"]").text() + '</li>')
+        // $('#row_'+randomID+' > td#tbl_goal ul.ul-flow').append('<li>'+ $("select[name=CPT_GOAL] option[value="+goal[i]+"]").text() + '<input type="hidden" name="h_CPT_GAPS_'+i+'" value="' +$("select[name=CPT_GOAL] option[value="+goal[i]+"]").val()+ '"/> </li>');
+        $('#row_'+randomID+' > td#tbl_goal > ul.ul-flow').append('<li>'+ $("select[name=CPT_GOAL] option[value="+goal[i]+"]").text() + '</li>')
     }
-    $('#tbl_goal').html(cpt_goal_choices[goal]);
-    $('#tbl_needs').html( $('select[name=CPT_GAPS] option[value='+gaps+']').text() );
-    $('#tbl_actions').html( $('select[name=CPT_ACTIONS] option[value='+actions+']').text() );
-    $('#tbl_services').html( $('select[name=CPT_SERVICES] option[value='+services+']').text() );
-    $('#tbl_repsonsible').html( $('select[name=CPT_RESPONSIBLE] option[value='+responsibl+']').text() );
-    $('#tbl_datecompleted').html(date);
-    $('#tbl_results').html( $('select[name=CPT_RESULTS] option[value='+results+']').text() );
-    $('#tbl_reasons').html(reasons);
-    $('#tbl_acts').html('<a href="#" class="removerow btn btn-xs btn-danger"><i class="fa fa-trash"></i> Remove</a>');
+    if(goal.length > 2){ $('#row_'+randomID+' > td#tbl_goal').append('</ul>'); }
+    $('#row_'+randomID+' > td#tbl_goal').append('<input type="hidden" name="h_CPT_GOAL" value="'+goal+'" />');
+    $('#row_'+randomID+' > td#tbl_needs').html( $('select[name=CPT_GAPS] option[value='+gaps+']').text() + '<input type="hidden" name="h_CPT_GAPS" value="'+gaps+'" />');
+    $('#row_'+randomID+' > td#tbl_actions').html( $('select[name=CPT_ACTIONS] option[value='+actions+']').text() + '<input type="hidden" name="h_CPT_ACTIONS" value="'+actions+'" />');
+    $('#row_'+randomID+' > td#tbl_services').html( $('select[name=CPT_SERVICES] option[value='+services+']').text() + '<input type="hidden" name="h_CPT_SERVICES" value="'+services+'" />' );
+    $('#row_'+randomID+' > td#tbl_repsonsible').html( $('select[name=CPT_RESPONSIBLE] option[value='+responsibl+']').text() + '<input type="hidden" name="h_CPT_RESPONSIBLE" value="'+responsibl+'" />');
+    $('#row_'+randomID+' > td#tbl_datecompleted').html(date + '<input type="hidden" name="h_CPT_DATE" value="'+date+'" />');
+    $('#row_'+randomID+' > td#tbl_results').html( $('select[name=CPT_RESULTS] option[value='+results+']').text() + '<input type="hidden" name="h_CPT_RESULTS" value="'+results+'" />');
+    $('#row_'+randomID+' td#tbl_reasons').html('<div>'+reasons+'</div>' + '<input type="hidden" name="h_CPT_RESULTS" value="'+results+'" />');
+    $('#row_'+randomID+' td#tbl_acts').html('<a href="#" class="removerow btn btn-xs btn-danger"><i class="fa fa-trash"></i> Remove</a>');
     
     console.log(
         'LOG: '+JSON.stringify({
             1: domain,
-            2: goal,
+            2: goal,    
             3: gaps,
             4: actions,
             5: services,
@@ -116,11 +69,13 @@ function AddRow() {
             9: reasons
         })
     )
-    $('.removerow').click(function (e) { 
+    $('#row_'+randomID+' > td#tbl_acts > .removerow').click(function (e) { 
         e.preventDefault();
-        $(this).closest('tr').empty();
+        $('#row_'+randomID).empty();
+        $('#row_'+randomID).remove();
+
+        // $(this).closest('tr').empty();
         // $(this).closest('tr').remove();
-        console.log('removerow');
     });
 
 }
